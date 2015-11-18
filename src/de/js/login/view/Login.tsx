19b7fab24771d0decde8918d7fc3d4734d7d07f2
Jörg Wasmeier <j.wasmeier@faba.de>
2015-11-18 19:02:57 +0100

    Login Typescript types

--- src/de/js/login/view/Login.tsx
+++ src/de/js/login/view/Login.tsx
@@ -1,13 +1,11 @@
 import * as React from 'react';
 import * as ReactDOM from 'react-dom';
+
 import MouseEvent = __React.MouseEvent;
 import SyntheticEvent = __React.SyntheticEvent;
-//import LoginUserEvent from "../controller/event/LoginUserEvent";
-
-import LoginUserEvent from "de/js/login/controller/event/LoginUserEvent";
-
 
 import {CircularProgress, Toggle, TextField, Paper, FlatButton} from 'material-ui';
+import LoginUserEvent from "../controller/event/LoginUserEvent";
 
 require("./Login.less");
 
@@ -24,41 +22,41 @@ export default class Login extends React.Component<{},{}> {
         busy?:boolean
     }
 
-    private onClickHandler(e:MouseEvent) {
-        console.log("onClickHandler");
+    private onClickHandler(e:MouseEvent):void {
         new LoginUserEvent(this.userName, this.passWord).dispatch();
-        this.setState({busy:true});
+        this.setState({busy: true});
     }
 
-    private changeUserNameHandler(e:SyntheticEvent) {
+    private changeUserNameHandler(e:SyntheticEvent):void {
         var input:HTMLInputElement = e.target as HTMLInputElement
         this.userName = input.value;
     }
 
-    private changePassWordHandler(e:SyntheticEvent) {
+    private changePassWordHandler(e:SyntheticEvent):void {
         var input:HTMLInputElement = e.target as HTMLInputElement
         this.passWord = input.value;
     }
 
-   private renderProgress() {
-        if ( this.state && this.state.busy ) {
+    private renderProgress():JSX.Element {
+        if (this.state && this.state.busy) {
             return (
                 <div className="busy">
-                    <CircularProgress mode="indeterminate" />
+                    <CircularProgress mode="indeterminate"/>
                 </div>
             )
         }
     }
 
-    render() {
+    render():JSX.Element {
         return (
-            <div className="center">
+            <div className={`center ${this.className}`}>
                 <Paper className="loginCard">
                     <Paper className="title" zDepth={0}>
                         <p>TIMEKEEPER</p>
                     </Paper>
 
                     <div className="content">
+
                         <TextField className="textField"
                                    floatingLabelText="Username"
                                    onChange={this.changeUserNameHandler}/>
@@ -72,9 +70,9 @@ export default class Login extends React.Component<{},{}> {
                                 value="toggleValue1"
                                 label="Auto login?!"/>
 
-                        <FlatButton label="LOGIN" className="loginBt" onClick={e => this.onClickHandler(e)}></FlatButton>
+                        <FlatButton label="LOGIN" className="loginBt"
+                                    onClick={e => this.onClickHandler(e)}></FlatButton>
                     </div>
-
 
                     {this.renderProgress()}
 
@@ -83,7 +81,7 @@ export default class Login extends React.Component<{},{}> {
         )
     }
 
-    public renderToDom(name:string = "container") {
+    public renderToDom(name:string = "container"):void {
         ReactDOM.render(<Login/>, document.getElementById(name));
     }
 }
